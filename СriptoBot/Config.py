d5530ab69b6de5060b62434f4b550ddcbe9f84b8
nikon561 <120553056+nikon561@users.noreply.github.com>
2023-01-22 11:32:52 +0300

    Add files via upload

---
+++ СriptoBot/Config.py
@@ -0,0 +1,6 @@
+TOKEN = "5875094217:AAGtgi4Ml5pfdypXdaoCbXorYuja17VN5go"
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB'
+}
---
+++ СriptoBot/Extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+from Config import keys
+class APIException(Exception):
+    pass
+class ConvertionException(Exception):
+    pass
+class CryptoConverter:
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество валюты{amount}')
+        if amount <= 0:
+            raise APIException(f'Невозможно конвертировать количество валюты меньше или равное 0')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        total_base = json.loads(r.content)[keys[quote]]
+        return total_base
+
